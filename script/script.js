0f9b6ab39c6d1c40d7acc97a9017d1aaabd117ee
Richard Marcell <richard.marcell8888@gmail.com>
2023-03-03 10:39:56 +0700

    perbaikan terakhit

--- script/script.js
+++ script/script.js
@@ -11,21 +11,26 @@ document.addEventListener("click", function (e) {
   }
 });
 
-
-
 // fungsi untuk menjalankan animasi
 const animation1 = lottie.loadAnimation({
-    container: document.getElementById('animasi1'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'asset/delivery-boy-5605460.json'
-  });
+  container: document.getElementById("animasi1"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "asset/delivery-boy-5605460.json",
+});
 
-  const animation2 = lottie.loadAnimation({
-    container: document.getElementById('animasi2'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'asset/woman-shopping-online.json'
-  });
+const animation2 = lottie.loadAnimation({
+  container: document.getElementById("animasi2"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "asset/woman-shopping-online.json",
+});
+
+
+
+
+
+
+
